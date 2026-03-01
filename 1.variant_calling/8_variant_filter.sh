7ef0ff610102d131b9eac686d99d06edf3aba434
fuqi2515 <127859036+fuqi2515@users.noreply.github.com>
2026-03-01 13:29:52 +0800

    Update 8_variant_filter.sh

--- 1.variant_calling/8_variant_filter.sh
+++ 1.variant_calling/8_variant_filter.sh
@@ -28,7 +28,7 @@ bcftools filter -i '(FMT/DP>=2 & FMT/RGQ>=10) | (FMT/DP>=2 & FMT/GQ>=10)' -S . -
 
 tabix suzukii_178_invariant_HQ.vcf.gz
 
-#step3: keep 18 autosome contigs and remove sites located in repetitive region
+#step3: keep 25 contigs (18 autosome + 7 X contigs) and remove sites located in repetitive region
 #filter variant sites
 vcftools --gzvcf suzukii_178_biallelic_HQ.vcf.gz --chr NW_023496800.1 --chr NW_023496812.1 --chr NW_023496807.1 --chr NW_023496808.1 --chr NW_023496835.1 --chr NW_023496836.1 --chr NW_023496837.1 --chr NW_023496816.1 --chr NW_023496817.1 --chr NW_023496819.1 --chr NC_050699.1 --chr NW_023496810.1 --chr NW_023496822.1 --chr NW_023496828.1 --chr NW_023496803.1 --chr NW_023496833.1 --chr NW_023496834.1 --chr NW_023496804.1 --chr NW_023496841.1 --chr NW_023496842.1 --chr NW_023496843.1 --chr NW_023496844.1 --chr NW_023496845.1 --chr NW_023496846.1 --chr NW_023496847.1 --exclude-positions repeat_region_postion.txt --max-missing 0.9 --remove-filtered LowQual --recode --stdout | bgzip -c >  suzukii_178_keep_25contigs_remove_mask_region_variants_HQ.vcf.gz
 
@@ -40,7 +40,7 @@ vcftools --gzvcf ALT_filter_suzukii_178_keep_25contigs_remove_mask_region_varian
 
 tabix ALT_filter_suzukii_178_keep_25contigs_remove_mask_region_INDEL_variants_HQ.vcf.gz
 
-#step4: keep 18 autosome contigs and remove sites located in repetitive region
+#step4: keep 25 contigs (18 autosome + 7 X contigs) and remove sites located in repetitive region
 #filter invariant sites
 vcftools --gzvcf suzukii_178_invariant_HQ.vcf.gz --chr NW_023496800.1 --chr NW_023496812.1 --chr NW_023496807.1 --chr NW_023496808.1 --chr NW_023496835.1 --chr NW_023496836.1 --chr NW_023496837.1 --chr NW_023496816.1 --chr NW_023496817.1 --chr NW_023496819.1 --chr NC_050699.1 --chr NW_023496810.1 --chr NW_023496822.1 --chr NW_023496828.1 --chrNW_023496803.1 --chr NW_023496833.1 --chr NW_023496834.1 --chr NW_023496804.1 --chr NW_023496841.1 --chr NW_023496842.1 --chr NW_023496843.1 --chr NW_023496844.1 --chr NW_023496845.1 --chr NW_023496846.1 --chr NW_023496847.1 --exclude-positions repeat_region_postion.txt --max-missing 0.9 --max-maf 0 --max-alleles 1 --remove-filtered LowQual --recode --stdout | bgzip -c >  suzukii_178_keep_25contigs_remove_mask_region_invariants_HQ.vcf.gz
 
