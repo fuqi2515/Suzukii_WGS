b4350aedf44adeaef382b82eb6624351c6d8a17e
fuqi2515 <127859036+fuqi2515@users.noreply.github.com>
2026-03-02 16:45:26 +0800

    Update 8_baypass.sh

--- 5.GEA/8_baypass.sh
+++ 5.GEA/8_baypass.sh
@@ -8,8 +8,11 @@
 ##SBATCH -o bypass.%j.log
 ##SBATCH --mem 4G
 
-date
+#This script is based on code from https://github.com/hkchi/LoStruct_RAD, modified by QiFu to suit the current analysis pipeline.
+#please cite below paper if you use this code.
+Huang K., Andrew R. L., Owens G. L., Ostevik K. L., Rieseberg L. H. Multiple chromosomal inversions contribute to adaptive divergence of a dune sunflower ecotype. Molecular Ecology 29, 2535-2549 (2020).
 
+date
 # Generate input file from VCF
 zcat ALT_filter_suzukii_178_keep_18contigs_plink_LD_prune_sampleid.vcf.gz | perl vcf2baypass.pl indi_pop_info.txt autosome_baypass
 
